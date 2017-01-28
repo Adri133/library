a5117efe2364797bb53b4c4498a96b3bd939a7dd
ADRI133 <pignon.adrien@gmail.com>
2017-01-28 14:56:26 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    // Database
+    'database' => [
+        'engine' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'library_dagpi',
+        'user' => 'root',
+        'password' => ''
+    ],
+
+    // Administrator auth
+    'admin' => ['root', '']
+];
